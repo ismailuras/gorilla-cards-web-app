82955b4c8cf8d1a2c7bfa643f7932bc01988c356
Ismail <katirciismail29@gmail.com>
2022-10-18 00:10:45 +0300

    prettier checked

--- src/components/features/showpassword/ShowPassword.js
+++ src/components/features/showpassword/ShowPassword.js
@@ -1,20 +1,24 @@
 import { useState } from "react";
-import { AiFillEye, AiOutlineEyeInvisible } from "react-icons/ai"
+import { AiFillEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 function ShowPassword({ children }) {
+  const [inputType, setInputType] = useState("password");
+  const isVisible = inputType === "text";
+  const handleVisible = () => {
+    setInputType((val) => {
+      if (val === "password") return "text";
+      return "password";
+    });
+  };
 
-   const [inputType, setInputType] = useState("password")
-   const isVisible = inputType === "text"
-   const handleVisible = () => {
-     setInputType((val) => {
-      if(val === "password") return "text"
-       return "password"
-     })
-   }
-
-  return <>{children(inputType)}
-  <button type="button" onClick={handleVisible}>{ isVisible ? <AiOutlineEyeInvisible /> : <AiFillEye /> }</button>
-  </>;
+  return (
+    <>
+      {children(inputType)}
+      <button type="button" onClick={handleVisible}>
+        {isVisible ? <AiOutlineEyeInvisible /> : <AiFillEye />}
+      </button>
+    </>
+  );
 }
 
 export default ShowPassword;
