b7b6f718c31ac82a5334f59815025ce107e2c5fd
Ismail <katirciismail29@gmail.com>
2022-10-18 00:51:56 +0300

    indicator color changed

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ function Button({ children, isLoading }) {
       disabled={isLoading}
       type="submit"
       className="bg-indigo-600 text-white text-xl py-2 px-6 my-10 rounded hover:bg-indigo-700">
-      {isLoading ? <Spinner /> : children}
+      {isLoading ? <Spinner isLoading={isLoading} /> : children}
     </button>
   );
 }
--- src/components/features/spinner/Spinner.js
+++ src/components/features/spinner/Spinner.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-function Spinner() {
+function Spinner({ isLoading }) {
   return (
-    <span className="bg-indigo-600 text-white py-2 px-6 my-10 rounded hover:bg-indigo-700">
+    <span
+      className={`${
+        isLoading
+          ? "opacity-70 text-white"
+          : "bg-indigo-600 text-white py-2 px-6 my-10 rounded hover:bg-indigo-700"
+      }`}>
       Loading...
     </span>
   );
