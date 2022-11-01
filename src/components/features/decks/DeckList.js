2ca2e5a5f88c81baadd85f608da94642228c5a42
Ismail <katirciismail29@gmail.com>
2022-11-01 05:45:02 +0300

    fix:#15 error messages fixed

--- src/components/features/decks/DeckList.js
+++ src/components/features/decks/DeckList.js
@@ -1,13 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDecks } from "stores/deckSlice";
-import { MdEditNote } from "react-icons/md";
-import { useState } from "react";
-import EditDeck from "components/features/decks/EditDeck";
-import MyModal from "../modal/MyModal";
 
 function DeckList() {
-  const [isEditDeckModalOpen, setEditDeckModal] = useState(false);
   const items = useSelector((state) => state.decks.items);
   const errorMessageOnFetched = useSelector((state) => state.decks.erroMessage);
   const dispatch = useDispatch();
@@ -15,10 +10,6 @@ function DeckList() {
   useEffect(() => {
     dispatch(fetchDecks());
   }, [dispatch]);
-
-  const openUpdateDeckModal = () => {
-    setEditDeckModal(true);
-  };
 
   return (
     <div className="flex flex-col items-start ml-10">
@@ -30,23 +21,13 @@ function DeckList() {
             <ul>
               <li className="w-full underline text-blue-600 text-xl flex justify-between">
                 {item.deckName}
-                <button onClick={openUpdateDeckModal}>
-                  <MdEditNote />
-                </button>
               </li>
             </ul>
             {errorMessageOnFetched && <p>Unexpected error occured.</p>}
           </div>
         ))
       )}
-      <div>
-        <MyModal
-          isOpen={isEditDeckModalOpen}
-          setOpen={setEditDeckModal}
-          title={"Edit Deck"}>
-          <EditDeck />
-        </MyModal>
-      </div>
+
       <button className="underline text-blue-600 rounded font-medium text-xl p-1 cursor-pointer">
         What am i?
       </button>
