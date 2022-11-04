06e93981ec83412556c9e9e32804a1a07395b4de
Ismail <katirciismail29@gmail.com>
2022-11-04 21:32:46 +0300

    feat:#17 delete feat. does not working, redux refactoring added

---
+++ src/components/features/decks/DeleteDeck.js
@@ -0,0 +1,37 @@
+import { useSelector, useDispatch } from "react-redux";
+import { deleteDeck } from "stores/deckSlice";
+import Button from "components/Button";
+import { showToast } from "helpers";
+
+function DeleteDeck({ closeDeleteModal }) {
+  const status = useSelector((state) => state.decks.createStatus);
+  const currentDeck = useSelector((state) => state.decks.currentDeck);
+  const { deckName } = currentDeck;
+  const dispatch = useDispatch();
+
+  const handleDeleteDeck = async () => {
+    try {
+      dispatch(deleteDeck());
+      showToast("Deleted", "success");
+      closeDeleteModal();
+    } catch (error) {
+      showToast("Error", "error");
+    }
+  };
+  return (
+    <div className="flex flex-col">
+      <div>
+        <p className="text-xl mb-10">
+          You are going to delete this <strong>{deckName}</strong>. Do you
+          confirm ?
+        </p>
+      </div>
+      <Button>Cancel</Button>
+      <Button onClick={handleDeleteDeck} disabled={status === "loading"}>
+        {status === "loading" ? "Loading" : "Delete Deck"}
+      </Button>
+    </div>
+  );
+}
+
+export default DeleteDeck;
