9697ec663365ad8c819dcb5308ef969bd3607b01
Can Küçükyılmaz <53147473+ivanfuzuli@users.noreply.github.com>
2022-11-06 02:01:45 +0300

    change bucket path

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>
